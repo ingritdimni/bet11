85f1202410f49792cea90ec1ee5b03ed3ca42e39
Frederico <FredericoMFalcao@hotmail.com>
2016-07-13 15:53:49 +0300

    Deleted sample / unncessary data.

--- data/_schema/1_group.sql
+++ data/_schema/1_group.sql
@@ -23,27 +23,3 @@ CREATE TABLE betting_group
 ##  SAMPLE DATA
 ##
 #########################
-
-INSERT INTO betting_group (id,
-	name,
-	date_time_podium_closing_bets,
-	points_for_match_winning_team,
-	points_for_match_correct_score,
-	points_for_team_in_podium,
-	points_for_correct_place_team_in_podium,
-	points_for_podium_best_player,
-	language,
-	group_url) 
-	VALUES (1, "ENVMA","2012-12-12 21:15:00",3,1,10,10,40,"PT","envma.dtdns.net");
-	
-INSERT INTO betting_group (id,
-	name,
-	date_time_podium_closing_bets,
-	points_for_match_winning_team,
-	points_for_match_correct_score,
-	points_for_team_in_podium,
-	points_for_correct_place_team_in_podium,
-	points_for_podium_best_player,
-	language,
-	group_url) 
-	VALUES (2, "Mustasilta & Friend's Betting Competition","2012-12-12 21:15:00",3,1,10,10,40,"EN","mfec.dtdns.net");
